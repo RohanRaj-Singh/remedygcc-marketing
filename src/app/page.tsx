fa1a151a511b6ba8aff879a017ba07f43204745c
RohanRaj-Singh <mrsinghdev13@gmail.com>
2026-03-14 03:51:15 +0500

    Setting clinics page as home temporarily

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
-import Hero from "../components/sections/Hero";
-import PartnersSlider from "../components/sections/PartnersSlider";
-import ProductSection from "../components/sections/ProductSection";
-import TherapistsGallery from "../components/sections/TherapistsGallery";
+// TEMPORARY: Clinics page set as homepage for testing
+// TODO: Revert to original homepage content when done
+import ClinicsPage from "./clinics/page";
+
+// Original imports (commented out for reference):
+// import Hero from "../components/sections/Hero";
+// import PartnersSlider from "../components/sections/PartnersSlider";
+// import ProductSection from "../components/sections/ProductSection";
+// import TherapistsGallery from "../components/sections/TherapistsGallery";
 
 export default function Home() {
   return (
-    <>
-      <Hero />
-      <PartnersSlider />
-      <ProductSection />
-      <TherapistsGallery />
-    </>
+    <ClinicsPage />
   );
+  
+  // Original return statement:
+  // return (
+  //   <>
+  //     <Hero />
+  //     <PartnersSlider />
+  //     <ProductSection />
+  //     <TherapistsGallery />
+  //   </>
+  // );
 }
